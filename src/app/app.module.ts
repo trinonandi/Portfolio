046c91b69874a0df01dfeb0ab4528821ac3bea16
Trinanjan Nandi <trino.nandi@gmail.com>
2023-08-11 19:06:20 -0700

    Create landing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomepageComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
