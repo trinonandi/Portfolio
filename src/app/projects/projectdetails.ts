1541189fc9dfb6ce2e5fb3061068cd9655f6279a
saibalmaiti <saibalmaiti23@gmail.com>
2023-10-11 10:41:19 +0530

    projects added with placeholders

---
+++ src/app/projects/projectdetails.ts
@@ -0,0 +1,6 @@
+export interface ProjectDetails {
+    imgUrl: String,
+    heading: String,
+    date: String,
+    details: String
+}
--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ProjectDetails } from './projectdetails';
+import { formatDate } from '@angular/common';
 
 @Component({
   selector: 'app-projects',
@@ -6,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./projects.component.css']
 })
 export class ProjectsComponent {
-
+  project1: ProjectDetails = {
+    imgUrl: "http://via.placeholder.com/640x360",
+    heading: "Lorem ipsum dolor sit amet",
+    date: "May 2, 2021",
+    details: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos numquam accusantium inventore earum exercitationem, quos facilis quisquam, possimus nam dolorem sapiente recusandae perferendis consequuntur impedit nesciunt veniam dicta, sed ipsa!"
+}
+projects = [this.project1]
 }
